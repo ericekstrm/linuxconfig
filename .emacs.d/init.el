4426f5ea4b59b172addbd4aaa6901f749f8d4e53
Eric Ekström <ericekstrm@gmail.com>
2023-04-05 10:46:50 +0200

    Switched the emacs config over to org mode with babel to compile to elisp.

---
+++ .emacs.d/init.el
@@ -0,0 +1,5 @@
+
+
+
+(require 'org)
+(org-babel-load-file (expand-file-name "~/linuxconfig/.emacs.d/emacs-config.org"))
--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ $lncom ~/linuxconfig/.ssh/config ~/.ssh/config
 $lncom ~/linuxconfig/.ssh/authorized_keys ~/.ssh/authorized_keys
 $lncom ~/linuxconfig/.profile ~/.profile
 $lncom ~/linuxconfig/.gitconfig ~/.gitconfig
+$lncom ~/linuxconfig/.emacs.d/init.el ~/.emacs.d/init.el
 
 if [ "$DESKTOP_SESSION" == "plasma" ]
 then
