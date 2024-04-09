12af5ce70790af8d109ff592760a2ea2de9bd1b2
Eric Ekström <ericekstrm@gmail.com>
2024-04-09 10:37:30 +0200

    better org bindings for evil mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,7 @@
    '("cacfbdd6dbabc3b03c4027fc6fb95adf2d846576ada447ee5c46a67686a1477b" "e684b575fcbb96e47785e410bf3f4d2540d2ab6f3a2968e83becb74968c46779" default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(zenburn-theme lsp-ivy lsp-mode restart-emacs evil-textobj-tree-sitter cmake-mode which-key use-package-chords undo-tree treemacs tree-sitter-langs telephone-line rainbow-delimiters powerline ivy-rich evil-surround evil-commentary evil-collection doom-themes doom-modeline counsel)))
+   '(evil-org-agenda evil-org zenburn-theme lsp-ivy lsp-mode restart-emacs evil-textobj-tree-sitter cmake-mode which-key use-package-chords undo-tree treemacs tree-sitter-langs telephone-line rainbow-delimiters powerline ivy-rich evil-surround evil-commentary evil-collection doom-themes doom-modeline counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
